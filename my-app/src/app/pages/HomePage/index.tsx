cca9dc6647315e287f1ddf4322cf40ec4bb1d6c2
jeong jun <jungjunjenon3047@gmail.com>
2022-12-12 16:08:20 +0900

    feature: PageLayout, Todo base

--- my-app/src/app/pages/HomePage/index.tsx
+++ my-app/src/app/pages/HomePage/index.tsx
@@ -1,14 +1,59 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import styled from 'styled-components';
+
+const Warp = styled.div`
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #e4e4e4;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Main = styled.div`
+  width: 400px;
+  height: 500px;
+  background-color: white;
+  margin: 0;
+  padding: 10px;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+`;
+
+const TodoList = styled.div``;
+
+const TodoItem = styled.div`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  width: 100%;
+  border-bottom: solid 1px black;
+`;
+
+const CheckBox = styled.input`
+  margin-right: 10px;
+`;
 
 export function HomePage() {
   return (
     <>
       <Helmet>
-        <title>HomePage</title>
-        <meta name="description" content="A Boilerplate application homepage" />
+        <title>Maian</title>
+        <meta name="description" content="To Do main임" />
       </Helmet>
-      <span>My HomePage</span>
+      <Warp>
+        <Main>
+          <Title>To-Do</Title>
+          <TodoList>
+            <TodoItem><CheckBox type= 'checkbox'></CheckBox>이거해야함</TodoItem>
+          </TodoList>
+        </Main>
+      </Warp>
     </>
   );
 }
